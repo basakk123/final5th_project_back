a59f3e39d82f40b69731e49400cdab4e4d112028
basakk123 <basakk@naver.com>
2022-12-05 11:13:36 +0900

    findUserRealnameById  컨트롤러에 권한 처리 추가

--- src/main/java/shop/mtcoding/final5th/web/UserApiController.java
+++ src/main/java/shop/mtcoding/final5th/web/UserApiController.java
@@ -1,5 +1,7 @@
 package shop.mtcoding.final5th.web;
 
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import shop.mtcoding.final5th.config.auth.LoginUser;
+import shop.mtcoding.final5th.config.exception.CustomApiException;
 import shop.mtcoding.final5th.dto.ResponseDto;
 import shop.mtcoding.final5th.dto.UserRespDto.UserRealnameRespDto;
 import shop.mtcoding.final5th.service.UserService;
@@ -21,10 +25,15 @@ public class UserApiController {
 
     private final UserService userService;
     private final Logger log = LoggerFactory.getLogger(getClass());
+    private final HttpSession session;
 
     @GetMapping("/user/userrealname/{userId}")
     public ResponseEntity<?> findUserRealnameById(@PathVariable Long userId) {
         log.debug("디버그 : findUserRealnameById 컨트롤러 실행됨");
+        LoginUser loginUser = (LoginUser) session.getAttribute("loginUser");
+        if (loginUser.getUserId() != userId) {
+            throw new CustomApiException("권한이 없습니다", HttpStatus.FORBIDDEN);
+        }
         UserRealnameRespDto userRealnameRespDto = userService.findUserRealnameById(userId);
         log.debug("디버그 : findUserRealnameById 컨트롤러 리턴 전");
         return new ResponseEntity<>(new ResponseDto<>(HttpStatus.OK, "유저네임보기 성공", userRealnameRespDto), HttpStatus.OK);
