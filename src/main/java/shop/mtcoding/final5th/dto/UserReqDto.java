1a0f6c5e1f812fb5b7b970231f47c47f4c95d463
paper.y <paper.yellow.op@gmail.com>
2022-12-08 05:43:40 +0900

    회원가입/이메일 인증, 서버 작동 안 됨. 백업용

--- src/main/java/shop/mtcoding/final5th/dto/UserReqDto.java
+++ src/main/java/shop/mtcoding/final5th/dto/UserReqDto.java
@@ -1,7 +1,14 @@
 package shop.mtcoding.final5th.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import shop.mtcoding.final5th.domain.user.User;
 
 public class UserReqDto {
 
@@ -10,5 +17,53 @@ public class UserReqDto {
     public static class LoginReqDto {
         private String username;
         private String password;
+    }
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Data
+    public static class JoinReqDto {
+        @Size(min = 2, max = 20)
+        @NotBlank
+        private String username;
+
+        @Size(min = 4, max = 20)
+        @NotBlank
+        private String userPassword;
+
+        @Size(min = 4, max = 50)
+        @NotBlank
+        private String userEmail;
+
+        private String userEmailConfirm = "confirmNot";
+
+        public User toEntity() {
+            User user = User.builder()
+                    .userName(username)
+                    .userPassword(userPassword)
+                    .userEmail(userEmail)
+                    .userEmailConfirm(userEmailConfirm)
+                    .build();
+            return user;
+        }
+    }
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Data
+    public class EmailCheckReqDto {
+
+        @Size(min = 2, max = 20)
+        @NotBlank
+        private String username;
+
+        @Size(min = 4, max = 20)
+        @NotBlank
+        private String userEmail;
+
+        @Size(min = 4, max = 20)
+        @NotBlank
+        private String userEmailConfirm;
+
     }
 }
--- src/main/java/shop/mtcoding/final5th/service/UserService.java
+++ src/main/java/shop/mtcoding/final5th/service/UserService.java
@@ -10,6 +10,9 @@ import lombok.RequiredArgsConstructor;
 import shop.mtcoding.final5th.config.exception.CustomApiException;
 import shop.mtcoding.final5th.domain.user.User;
 import shop.mtcoding.final5th.domain.user.UserRepository;
+import shop.mtcoding.final5th.dto.UserReqDto.EmailCheckReqDto;
+import shop.mtcoding.final5th.dto.UserReqDto.JoinReqDto;
+import shop.mtcoding.final5th.dto.UserReqDto.LoginReqDto;
 import shop.mtcoding.final5th.dto.UserRespDto.UserRealnameRespDto;
 
 @Transactional(readOnly = true)
@@ -26,5 +29,24 @@ public class UserService {
                 .orElseThrow(() -> new CustomApiException("해당 유저가 없습니다", HttpStatus.BAD_REQUEST));
         log.debug("디버그 : findUserRealnameById 서비스 리턴 전");
         return new UserRealnameRespDto(userPS);
+    }
+
+    public void 회원가입(JoinReqDto dto) {
+        userRepository.save(dto.toEntity());
+
+    }
+
+    public User 로그인(LoginReqDto dto) {
+        User userEntity = userRepository.mLogin(dto.getUsername(), dto.getPassword());
+        return userEntity;
+    }
+
+    public User 이메일인증확인(EmailCheckReqDto dto) {
+        User userEntity = userRepository.mCheck(dto.getUsername(), dto.getUserEmailConfirm());
+        return userEntity;
+    }
+
+    public void 이메일인증업데이트(EmailCheckReqDto dto) {
+        userRepository.mUpdate(dto.getUsername());
     }
 }
