3512cc0b3d60573fe27860daee86eed537595c4d
basakk123 <basakk@naver.com>
2022-12-15 10:31:13 +0900

    토큰 verify 테스트 완료

--- src/test/java/shop/mtcoding/final5th/config/jwt/JwtProcessTest.java
+++ src/test/java/shop/mtcoding/final5th/config/jwt/JwtProcessTest.java
@@ -18,10 +18,10 @@ public class JwtProcessTest {
     @Test
     public void verify_test() throws Exception {
         // given
-
+        String jwtToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzc2FyIiwiZXhwIjoxNjcxOTMxNzMyLCJ1c2VyTmFtZSI6InNzYXIiLCJ1c2VySWQiOjF9.zrKU5j2ZBLkfJRqVhK6s8c5MrfjSpR1KTpjfNi2uYjf_Km69X9GM4jF-bfvmpUa4rRyQO-JFyJWVp02S8_QfeA";
         // when
-
+        Long userId = JwtProcess.verify(jwtToken);
         // then
-
+        System.out.println(userId);
     }
 }
